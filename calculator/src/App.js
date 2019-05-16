8923764e1c5d19b597e6242a0a400dc19af2acaa
Damini Ganesh <damini.ganesh@covetool.com>
2019-05-16 10:31:01 -0400

    basic calculator style

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display" />
+      <div className="buttons">
+        <div className="button top">AC</div>
+        <div className="button top">±</div>
+        <div className="button top">%</div>
+        <div className="button right">÷</div>
+        <div className="button classic">7</div>
+        <div className="button classic">8</div>
+        <div className="button classic">9</div>
+        <div className="button right">×</div>
+        <div className="button classic">4</div>
+        <div className="button classic">5</div>
+        <div className="button classic">6</div>
+        <div className="button right">–</div>
+        <div className="button classic">1</div>
+        <div className="button classic">2</div>
+        <div className="button classic">3</div>
+        <div className="button right">+</div>
+        <div className="button classic zero">0</div>
+        <div className="button classic">.</div>
+        <div className="button right">=</div>
+      </div>
     </div>
   );
 }
